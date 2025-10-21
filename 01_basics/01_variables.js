dc0ad2efa4c184cbaf20b72fbc4f53363ca4a8bb
Ayushyen <123053083+Ayushyen@users.noreply.github.com>
2025-10-21 14:36:06 +0000

    datatypes and notes updated

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -14,10 +14,9 @@ console.table([EmployeeID,EmployeeDepartment,EmployeeName,baseLocation,baseState
 
 /*
 js initially had var used as way to declare variables but
-it didn't have any scope like local variables have and are limited
-within the {} but variables declared using 'var' had somewhat
-global scope, thus a change to it will change it everywhere,
+it had function scope, can be used anywhere in the funtion,
+, thus a change to it will change it everywhere,
 not used anymore in modern code but still there to support
-some legacy code, note removed from js, but we will use only
-const and let !!! 
+some legacy code, not removed from js, but we will use only
+const and let as they have block scope {} !!! (var doesn't exist for us) 
 */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+2 versions of js: modern and old, to provide context to the 
+execution environment, use strict is used in the file to say I'm
+based on new JS convention and this will enable all the features
+of new JS like arrow functions, etc., to get old JS, there's no keyword
+as of now, also by default it detects the context but it's a 
+good code practice to mention it 
+*/
+
+//alert(3+3) //will work in browser with a prompt box
+//won't work here in node but there's a way to do this here too
+
+/*
+number => limit is around 2^53, mostly this is used as this is more than enough
+bigint => more capacity; useful where higher numbers are dealt like HFT, numerous requests are handled
+string => anything defined in "" or ''; empty string is not null or undefined!!!!
+null => standalone value; type of object(debated: blunder or gr8 move?); used to specify null value, where we can't define 0 as it's a critical point
+undefined => a special data type in js, default values for variable or any operation that leads to it
+boolean => true or false
+symbol => to determine uniqueness for components; will see with react and figma stuff
+object => will see later
+*/
+console.log(typeof null);
+console.log(typeof undefined);
